fe0e631895874d2ce36847f19fe90920aef2dffb
Victor Nunes <up201907226@edu.fe.up.pt>
2022-04-03 15:29:40 +0100

    movement logic done

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ testBoard = [
     [0, 4, 0, 7, 6, 8],
     [0, 4, 4, 6, 6, 8],
     [0, 3, 3, 3, 8, 8],
-    [0, 0, 0, 3, 0, 0],
+    [1, 0, 0, 3, 0, 0],
 ]
 
 def main():
@@ -72,10 +72,10 @@ class LShapedFigure:
     
 
 class Operation(Enum):
-    MOVE_UP = 0
-    MOVE_RIGHT = 1
-    MOVE_DOWN = 2
-    MOVE_LEFT = 3
+    MOVE_UP = "up"
+    MOVE_RIGHT = "right"
+    MOVE_DOWN = "down"
+    MOVE_LEFT = "left"
 
 
 def Lvisited(board, x, y):
@@ -87,38 +87,38 @@ def possibleOperations(board, position):
 
     #Check if it's possible to move up
     if position.y + 1 < len(board) and (board[position.x][position.y + 1] != 2) and not Lvisited(board, position.x, position.y + 1):
-        operations.append(Operation.MOVE_UP)
+        operations.append(Operation.MOVE_RIGHT)
 
     #Check if it's possible to move right
     if position.x + 1 < len(board) and (board[position.x + 1][position.y] != 2) and not Lvisited(board, position.x + 1, position.y):
-        operations.append(Operation.MOVE_RIGHT)
+        operations.append(Operation.MOVE_DOWN)
     
     #Check if it's possible to move down
     if position.y - 1 >= 0 and (board[position.x][position.y - 1] != 2) and not Lvisited(board, position.x, position.y - 1):
-        operations.append(Operation.MOVE_DOWN)
+        operations.append(Operation.MOVE_LEFT)
 
     #Check if it's possible to move left
     if position.x - 1 >= 0 and (board[position.x - 1][position.y] != 2) and not Lvisited(board, position.x - 1, position.y):
-        operations.append(Operation.MOVE_LEFT)
+        operations.append(Operation.MOVE_UP)
 
     return operations
 
-def operation(board, position, op):
+def makeMove(board, position, op):
     board[position.x][position.y] = 2
     #TODO: Add the l in the visited list
-    if op == Operation.MOVE_UP:
+    if op == Operation.MOVE_RIGHT:
         position.y += 1
-    if op == Operation.MOVE_LEFT:
+    if op == Operation.MOVE_DOWN:
         position.x += 1
-    if op == Operation.MOVE_DOWN:
+    if op == Operation.MOVE_LEFT:
         position.y -= 1
-    if op == Operation.MOVE_DOWN:
+    if op == Operation.MOVE_UP:
         position.x -= 1
     board[position.x][position.y] = 1
 
 def gameOver(board):
     n = len(board)
-    return board[n - 1][n - 1] == 1
+    return board[0][len(board) - 1] == 1
 
 def humanPlay(board):
 
@@ -129,30 +129,50 @@ def humanPlay(board):
             if element >= 3:
                 l_figures.add(LShapedFigure(element))
 
-    position = Position()
+    position = Position(len(board) - 1 ,0)
+
+    print("x = ", position.x)
+    print("y = ", position.y)
 
     while True:
         printBoard(board)
+        print("x = ", position.x)
+        print("y = ", position.y)
         possibleOps = possibleOperations(board, position)
 
-        print("Choose your move:\n")
-        print("1. Move up\n")
-        print("2. Move right\n")
-        print("3. Move down\n")
-        print("4. Move left\n\n")
-
-        print("Possible moves: ")
+        print("\nPossible moves: ")
         for op in possibleOps:
-            print(op + " ")
+            print(op)
+
         print("\n\n")
 
-        input = int(input)
+        while True:
+
+            ans = input()
+
+            if (ans == "up"):
+                chosen_move = Operation.MOVE_UP
+
+            if (ans == "right"):
+                chosen_move = Operation.MOVE_RIGHT
+
+            if (ans == "down"):
+                chosen_move = Operation.MOVE_DOWN
+
+            if (ans == "left"):
+                chosen_move = Operation.MOVE_LEFT
+
+
+            if (chosen_move in possibleOps):
+                break
+
+        makeMove(board, position, chosen_move) 
 
         '''
         move = getKeyPress()
         
         if move in possibleOps:
-            operation(board, position, move)
+            makeMove(board, position, move)
         '''
 
         if gameOver(board):
