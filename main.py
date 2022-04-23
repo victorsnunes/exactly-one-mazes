321a8215201aa690f6fb9fcdee2431c0f30e61db
Pedro <pedro.rc12@hotmail.com>
2022-04-23 18:59:44 +0100

    Just a small tweak
    Now it shows the result found

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from structures import Board
 from screen import Screen
 import pygame
 from search_algorithms import humanPlay, breadthSearch, depthSearch, greedySearch, aStarAlgorithm
+from utils import getKeyPress, Operation
 
 def main():
     screen = Screen()
@@ -39,6 +40,12 @@ def main():
     elif selected == "6":
         aStarAlgorithm(board, screen)
         
+    # Just to show the result in the end  
+    while True:
+        op = getKeyPress()
+        if(op == Operation.QUIT):
+            break
+    
     pygame.quit()
     print("quitting...")
 
