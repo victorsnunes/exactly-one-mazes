a7708a8a44a97e2e8d25e84473dca57ae53d9421
Victor Nunes <victorsnunes99@gmail.com>
2022-06-07 12:38:23 +0100

    improvs

--- sarsa.py
+++ sarsa.py
@@ -76,3 +76,27 @@ for episode in range(train_episodes):
 print("reward/episodes = " + reward/train_episodes)
 
 print(Q)
+
+total_epochs, total_penalties = 0, 0
+episodes = 100
+
+state = env.reset()
+epochs, penalties, reward = 0, 0, 0
+
+done = False
+
+while not done:
+    env.render()
+    action = np.argmax(Q[state.get_obs()])
+    state, reward, done, info = env.step(action)
+    print(action)
+    if reward == -10:
+        penalties += 1
+    epochs += 1
+total_penalties += penalties
+total_epochs += epochs
+
+
+print(f"Results after {episodes} episodes:")
+print(f"Average timesteps per episode: {total_epochs / episodes}")
+print(f"Average penalties per episode: {total_penalties / episodes}")
